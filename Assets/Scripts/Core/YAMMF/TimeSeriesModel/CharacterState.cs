dbdb43d270bafd0e640bfc135708e765a388a89c
Soeren Frohne <s.frohne@unitybox.de>
2020-10-08 17:08:32 +0200

    Fill Repo

---
+++ Assets/Scripts/Core/YAMMF/TimeSeriesModel/CharacterState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Core.YAMMF.TimeSeriesModel
+{
+    /**
+     * The CharacterState represents the current state of the character at frame i.
+     */
+    public class CharacterState
+    {
+    }
+}
---
+++ Assets/Scripts/Core/YAMMF/TimeSeriesModel/Input.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core.YAMMF.TimeSeriesModel
+{
+    /**
+     * The time-series model is predicting the state variables of the character, ball, etc. for a frame i+1 given
+     * those in the current frame i. The complete input vector consists of five components:
+     * CharacterState, ControlVariables, ConditioningFeatures, OpponentInformation and LocalMotionPhases.
+     */
+    public class Input
+    {
+        public CharacterState characterState;
+    }
+}
